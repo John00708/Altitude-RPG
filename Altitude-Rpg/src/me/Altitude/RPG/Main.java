95471de41a538e412b88750ca8daf2f3709fba8f
John00708 <john.andrews007@gmail.com>
2014-09-19 22:09:02 +0200

    First Commit!!!

---
+++ Altitude-Rpg/src/me/Altitude/RPG/Main.java
@@ -0,0 +1,9 @@
+package me.Altitude.RPG;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	public void onEnable(){
+		this.getLogger().info("Altitude's RPG has been enabled!!!");
+	}
+}
